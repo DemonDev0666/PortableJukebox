55dfebd3181f9ff7d6973cefd00fdac9a5373354
DemonDev0666 <DemonDev0666@gmail.com>
2024-10-07 00:14:25 -0400

    Updated ModItems File to test Discord Channel Updates

--- src/main/java/net/demondev/portablejukebox/item/ModItems.java
+++ src/main/java/net/demondev/portablejukebox/item/ModItems.java
@@ -1,4 +1,16 @@
 package net.demondev.portablejukebox.item;
 
+import net.demondev.portablejukebox.PortableJukebox;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PortableJukebox.MOD_ID);
+
+public static final RegistryObject<Item> PORTABLE_JUKEBOX = ITEMS.register("portable_jukebox",
+        () -> new Item(new Item.Properties()));
+
+
 }
